c26d6d314bd4579d83c3898bb68748fb30c5a64f
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-05-12 11:36:21 -0400

    Add calling seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CategorySeeder::class,
+            ProductSeeder::class,
+            VariationSeeder::class,
         ]);
     }
 }
