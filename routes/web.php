4cfb9edcf719ccda00d2c2ae126daedd6e3997c8
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-05-14 12:37:33 -0400

    Create a category product page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CartIndexController;
+use App\Http\Controllers\CategoryShowController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductShowController;
 use App\Http\Controllers\ProfileController;
@@ -8,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 Route::get('/cart', CartIndexController::class)->name('cart');
+Route::get('/categories/{category:slug}', CategoryShowController::class)->name('cart');
 Route::get('/products/{product:slug}', ProductShowController::class);
 
 Route::get('/dashboard', function () {
