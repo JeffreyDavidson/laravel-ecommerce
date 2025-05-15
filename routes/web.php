d622281c3d30e3619e1ed712b7711ac27e2a1080
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-05-15 09:55:12 -0400

    Remove duplicated route name

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('home');
 Route::get('/cart', CartIndexController::class)->name('cart');
 Route::get('/checkout', CheckoutIndexController::class)->middleware(RedirectIfCartEmpty::class)->name('checkout');
-Route::get('/categories/{category:slug}', CategoryShowController::class)->name('cart');
+Route::get('/categories/{category:slug}', CategoryShowController::class);
 Route::get('/products/{product:slug}', ProductShowController::class);
 
 Route::get('/dashboard', function () {
