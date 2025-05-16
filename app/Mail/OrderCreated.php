9a44e8a628986085e4ebb523e4ca923429b5929d
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-05-16 14:21:45 -0400

    Fix typos

--- app/Mail/OrderCreated.php
+++ app/Mail/OrderCreated.php
@@ -37,7 +37,7 @@ class OrderCreated extends Mailable
     public function content(): Content
     {
         return new Content(
-            markdown: 'emails.order.created',
+            markdown: 'emails.orders.created',
         );
     }
 
