303b26b17bf8f54bb157683456aae1f3b29e30d6
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2025-05-16 11:26:21 -0400

    Send email when status is updated

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use App\Observers\OrderObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Str;
 
+#[ObservedBy(OrderObserver::class)]
 class Order extends Model
 {
     public $fillable = [
